6d0566cbe13da236c55a8bf503486b0286c062cf
roxyzc <roxyzc12@gmail.com>
2023-03-19 21:07:21 +0700

    update

--- latihan2.php
+++
@@ -1,4 +0,0 @@
-<?php
-if (isset($_GET["nama"])) {
-    echo "Selamat Datang " . $_GET["nama"];
-}
--- php_dasar1.php
+++ php_dasar1.php
@@ -3,22 +3,18 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Dasar</title>
 </head>
 
 <body>
     <h1>Belajar PHP Dasar</h1>
     <?php
-    echo "<p>Hello World</p>";
-    ?>
-    <h3>Menggunakan Variable</h3>
-    <?php
-    $nim = "312110169";
-    $nama = 'Ridho Pikriyansyah';
-    echo "NIM : " . $nim . "<br>";
-    echo "Nama : $nama";
+    echo "Hello World<br>";
+
+    $nim = "3121101169";
+    $nama = "roxyzc";
+    echo "Nim: " . $nim . "<br>";
+    echo "Nama: " . $nama;
     ?>
 </body>
 
